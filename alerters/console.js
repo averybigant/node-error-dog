56e55958d635d2dcc9c3631addef109847e7f73d
Renbi.YU <averybigant@gmail.com>
2015-04-23 17:48:48 +0800

    Added error message output limit option for console alerter

--- alerters/console.js
+++ alerters/console.js
@@ -7,12 +7,20 @@ exports.init = function(emitter, target, log, settings) {
 };
 
 function Console(emitter, target, log, settings) {
+  var limit = settings.limit || -1;
+  var limit_info = "";
+
   emitter.on('alert', function(level, lines, extra) {
+    if (limit > 0 && lines.length > limit) {
+        lines = lines.slice(lines.length - limit, lines.length);
+        limit_info = util.format("(below is the last %d of them) ", limit);
+    }
     console.log(util.format(
-      '%s :: level(%d) :: %s :: %s',
+      '%s :: level(%d) :: %s %s:: %s',
       target.name,
       level,
       extra,
+      limit_info,
       lines.join('\n')
     ));
   });
